5b556692286cc1c44ff7e072f5d9ccf3c2d2a44a
beatriceToader <acribiebea@gmail.com>
2024-05-23 23:00:41 +0300

    first version of the quiz

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp2bd2f15f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
